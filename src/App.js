114266adbb6a96523fd0f75d11990c21aaeece01
Abhishek Kumar <Abhishek.kumar@beehyv.com>
2023-11-06 16:52:24 +0530

    added utility file and updated transalted.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import "./App.css";
-import Header from "./Components/Header"
+import Header from "./Components/Header";
 import Home from "./Home";
 import Translate from "./Components/Translate";
 import Age from "./Components/Age";
@@ -12,18 +17,23 @@ import NeedHelp from "./Components/NeedHelp";
 function App() {
   return (
     <div className="App">
-    <Router>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home key="home" />} />
-        <Route path="/bhashini" element={<Translate />} />
-        <Route path="/age" element={<Age />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/language" element={<Language />} />
-        <Route path="/card" element={<Card />} />
-        <Route path="/needHelp" element={<NeedHelp />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home key="home" />} />
+          <Route path="/bhashini" element={<Translate />} />
+          <Route path="/age" element={<Age />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/language" element={<Language />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/needHelp" element={<NeedHelp />} />
+          <Route
+            path="/translate/:sourceLanguage/:targetLanguage"
+            // path="/translate"
+            element={[<Translate />]}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
