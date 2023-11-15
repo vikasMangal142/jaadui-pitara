ae5ae1c818f4b723f197c23c3aa61b46404f2606
Abhishek Kumar <Abhishek.kumar@beehyv.com>
2023-11-15 17:06:35 +0530

    Added UsingRegex.js file which encodes config2.json and translates the encoded string entirely using UtilitiesEntireCard.js and the decodes translated string and stores the data in local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Home";
@@ -15,34 +11,38 @@ import NeedHelp from "./Components/NeedHelp";
 import { HeaderProvider } from "./Context/HeaderContext";
 import RouteChangeListener from "./RouteChangeListener";
 import Callback from "./Components/Callback";
+import UsingRegex from "./Components/UsingRegex";
 
 function App() {
   return (
     <div className="App">
       <HeaderProvider>
-      <Router>
-        <RouteChangeListener />
-        <Header />
-        <Routes>
-
-          <Route path="/" element={<Home key="home" />} />
-          <Route path="/bhashini" element={<Translate />} />
-          <Route path="/age" element={<Age />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/language" element={<Language />} />
-          <Route path="/card" element={<Card />} />
-          <Route path="/needHelp" element={<NeedHelp />} />
-          <Route
-            path="/translate/:sourceLanguage/:targetLanguage"
-            // path="/translate"
-            element={[<Translate />]}
-          />
-                      <Route
+        <Router>
+          <RouteChangeListener />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home key="home" />} />
+            <Route path="/bhashini" element={<Translate />} />
+            <Route path="/age" element={<Age />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/language" element={<Language />} />
+            <Route path="/card" element={<Card />} />
+            <Route path="/needHelp" element={<NeedHelp />} />
+            <Route
+              path="/translate/:sourceLanguage/:targetLanguage"
+              // path="/translate"
+              element={[<Translate />]}
+            />
+            <Route
               path="/translate/demo/:sourceLanguage/:targetLanguage"
               element={[<Callback />]}
             />
-        </Routes>
-      </Router>
+            <Route
+              path="/translate/usingRegex/:targetLanguage"
+              element={[<UsingRegex />]}
+            />
+          </Routes>
+        </Router>
       </HeaderProvider>
     </div>
   );
---
+++ src/Components/UsingRegex.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { translate1 } from "./UtilitiesEntireCard";
+import jsonData from "../config2.json";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+let namesOrder = ["category", "title", "Tasks"];
+let arrayOfOriginalUnchangedKeys = [];
+// let cardsIndex = [];
+let parsedOutput = [];
+
+function divider(str, arr, pattern) {
+  let i = 0;
+  let indexesOfPattern = [];
+  while (i !== -1) {
+    i = str.indexOf(pattern, i);
+    console.log("i", i + 1);
+    console.log("i", str[i + 1]);
+    if (i === -1) break;
+    indexesOfPattern.push(i + 2);
+    i += 2;
+  }
+
+  console.log("noOfDelimiter", indexesOfPattern.length);
+  for (let i = 0; i < indexesOfPattern.length; i++) {
+    if (i === indexesOfPattern.length - 1)
+      arr.push(str.substr(indexesOfPattern[i] + 1));
+    else
+      arr.push(
+        str.substr(
+          indexesOfPattern[i] + 1,
+          indexesOfPattern[i + 1] - indexesOfPattern[i] - 3
+        )
+      );
+  }
+}
+// Alwayas remember to put "Tasks" at the end of JSON card data.
+
+function decode(str) {
+  let cards = [];
+  divider(str, cards, "(@)");
+
+  cards.forEach((card) => {
+    console.log(card);
+
+    let names = [];
+    divider(card, names, "($)");
+    names.forEach((name) => console.log(name));
+    let obj = {};
+    for (let i = 0; i < names.length - 1; i++) obj[namesOrder[i]] = names[i];
+
+    let tasks = [];
+    obj[namesOrder[names.length - 1]] = [];
+    divider(names[names.length - 1], tasks, "(#)");
+    tasks.forEach((task) => {
+      console.log("task : ", task);
+      obj[namesOrder[names.length - 1]].push(task);
+    });
+    parsedOutput.push(obj);
+  });
+}
+
+function UsingRegex() {
+  const { targetLanguage } = useParams();
+  const [translatedCardsDataEncoded, setTranslatedCardsDataEncoded] =
+    useState();
+
+  let str = "";
+  jsonData.cards.forEach((card) => {
+    str += "(@)";
+    let names = Object.keys(card);
+    arrayOfOriginalUnchangedKeys.push({
+      categoryInEnglish: card.category,
+      ageGroup: card.ageGroup,
+      imageUrl: card.imageUrl,
+    });
+    names.forEach((name) => {
+      if (name === "ageGroup" || name === "imageUrl") return;
+      str += "($)";
+      if (name !== "Tasks") {
+        str += card[name];
+      } else {
+        card["Tasks"].forEach((task) => {
+          str += "(#)";
+          str += task.Action;
+          // str+="(#)";
+        });
+      }
+      // str+="($)";
+    });
+    // str+="(@)";
+  });
+  console.log("Encoded Cards Data : ", str);
+
+  translate1(str, targetLanguage, setTranslatedCardsDataEncoded);
+
+  useEffect(() => {
+    parsedOutput = [];
+    if (translatedCardsDataEncoded) decode(translatedCardsDataEncoded);
+
+    const objectToBeSavedInLocalStorage = {};
+    for (let i = 0; i < parsedOutput.length; i++) {
+      let cardData = parsedOutput[i];
+      cardData = { ...cardData, ...arrayOfOriginalUnchangedKeys[i] };
+      console.log("cardData : ", cardData);
+      if (!objectToBeSavedInLocalStorage[cardData.categoryInEnglish])
+        objectToBeSavedInLocalStorage[cardData.categoryInEnglish] = [];
+      objectToBeSavedInLocalStorage[cardData.categoryInEnglish].push(cardData);
+    }
+    let keysOfCategoriesForLocalStorage = Object.keys(
+      objectToBeSavedInLocalStorage
+    );
+    keysOfCategoriesForLocalStorage.forEach((key) => {
+      localStorage.setItem(
+        key,
+        JSON.stringify(objectToBeSavedInLocalStorage[key])
+      );
+    });
+  }, [translatedCardsDataEncoded]);
+}
+
+export default UsingRegex;
