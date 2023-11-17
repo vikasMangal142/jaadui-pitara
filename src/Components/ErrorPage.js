e810b9b61200c0261461345979968657f37f9b28
Suraj Majumdar <suraj.majumdar@beehyv.com>
2023-11-17 12:27:59 +0530

    Added ErrorPage UserDetails and edited Card

---
+++ src/Components/ErrorPage.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function ErrorPage() {
+  const [seconds, setSeconds] = useState(5);
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    // Clear the interval when the countdown reaches 0
+    if (seconds === 0) {
+      clearInterval(interval);
+    }
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [seconds]);
+
+  useEffect(() => {
+    // Redirect or perform any action when countdown reaches 0
+    if (seconds === 0) {
+      //   navigate("/userInfo");
+      console.log("Countdown completed!");
+    }
+  }, [seconds]);
+
+  const tryAgainHandler = () => {
+    // e.preventDefault();
+  };
+  return (
+    <div className="container dFlexAICenterJCCenter my-4 flex-1">
+      <div className="container selection-buttons-outer-container">
+        <div className="mt-4 text-center p-3 selection-header">
+          <h1 className="fw-bold display-5">
+            Please try again in {seconds} seconds
+          </h1>
+        </div>
+        <div className="container m-2 flex-lg-row selection-buttons">
+          <button
+            type="button"
+            onClick={tryAgainHandler}
+            className="btn btn-success cards-button"
+          >
+            Try again
+          </button>
+        </div>
+        <button
+          type="button"
+          onClick={handleBackClick}
+          className="mt-2 mb-4 btn btn-danger cards-button"
+        >
+          &#60; Back
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
---
+++ src/Components/UserInfo.js
@@ -0,0 +1,94 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import HeaderContext from "../Context/HeaderContext";
+
+function UserInfo() {
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo, setPageCount } = useContext(HeaderContext);
+
+  const [data, setData] = useState({
+    userName: "",
+    userContact: "",
+  });
+
+  useEffect(() => {
+    setPageCount(2);
+    console.log("User Info", userInfo);
+  }, []);
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserInfo(data);
+    localStorage.setItem("userInfo", JSON.stringify(data));
+    navigate("/language");
+  };
+
+  return (
+    <>
+      <div className="container dFlexAICenterJCCenter my-4 flex-1 p-1">
+        <div className="container selection-buttons-outer-container">
+          <div className="mt-4 text-center p-3 selection-header">
+            <h1 className="fw-bold display-5">Please enter your details</h1>
+          </div>
+
+          <div className="container m-1 px-4 flex-lg-row">
+            <form onSubmit={submitHandler}>
+              <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="userName"
+                  name="userName"
+                  placeholder="Your Name"
+                  onChange={changeHandler}
+                  value={userInfo.userName !== null ? userInfo.userName : ""}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Contact No.</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="userContact"
+                  name="userContact"
+                  placeholder="Your Contact No."
+                  pattern="[1-9][0-9]{9}"
+                  title="Please enter a valid contact number."
+                  onChange={changeHandler}
+                  value={
+                    userInfo.userContact !== null ? userInfo.userContact : ""
+                  }
+                  required
+                />
+              </div>
+              <div className="dFlexAICenterJCCenter justify-content-between">
+                <button
+                  type="button"
+                  onClick={handleBackClick}
+                  className="my-4 btn btn-danger"
+                >
+                  &#60; Back
+                </button>
+                <button type="submit" className="my-4 btn btn-success">
+                  Next >
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserInfo;
